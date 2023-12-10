521e9f927075b4e8d523a7836ecd3a4eb9a45507
Sai Kyaw Ye Myint <saikyawyemyint@acedatasystems.com>
2023-12-10 11:38:50 +0630

    First Commit

--- spring-api/src/main/java/com/example/springapi/student/StudentService.java
+++ spring-api/src/main/java/com/example/springapi/student/StudentService.java
@@ -1,2 +1,22 @@
-package com.example.springapi.student;public class StudentService {
+package com.example.springapi.student;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    public List<Student> getStudentList(){
+        return List.of(
+                new Student(
+                        "Marvin",
+                        "marvinskiller1993@gmail.com",
+                        30,
+                        LocalDate.now()
+                )
+        );
+    }
 }
